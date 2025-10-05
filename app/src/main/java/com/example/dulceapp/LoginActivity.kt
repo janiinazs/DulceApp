3891bc65b9206bc484bb50773a775943dfd29245
MelinaMacias5 <137859778+MelinaMacias5@users.noreply.github.com>
2025-10-05 18:13:54 -0500

    Update LoginActivity.kt

--- app/src/main/java/com/example/dulceapp/LoginActivity.kt
+++ app/src/main/java/com/example/dulceapp/LoginActivity.kt
@@ -19,7 +19,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         // Aplicar padding para barras del sistema
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutPrincipalLogin)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets

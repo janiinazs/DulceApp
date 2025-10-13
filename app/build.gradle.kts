bbb18eb14e3625622273a4d20f790821ea7934eb
jpat05 <jpatricio_05@hotmail.com>
2025-10-13 14:52:04 -0500

    database half integration2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
 
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     kapt(libs.room.compiler)
--- app/src/main/java/com/example/dulceapp/User.kt
+++ app/src/main/java/com/example/dulceapp/User.kt
@@ -4,4 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
-data class User(@PrimaryKey(autoGenerate = true) val id: Long = 0, val username: String, val email: String, val password: String)
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val username: String, val email: String,
+    val password: String)
